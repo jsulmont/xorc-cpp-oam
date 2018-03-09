a355680a564cbdfd7cb816d2717363b2355d6c86
stefan <stefan.graenitz@gmail.com>
2018-03-09 19:10:35 +0100

    XRAY2-10 [oamtestkit] Implement protocol: receive bytecode on stdin, send results to stdout
    Run with:
    $ cd oam-compiler
    $ ./_build/default/testkit/testkit.exe exec -s arithmetic "path/to/cpp-oam-ninja/tests/oamtestkit-driver/oamTestkitDriver" -verb

--- include/cppoam/hello.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace cppoam {
-
-void hello();
-
-}
--- tests/oamtestkit-driver/Driver.cpp
+++ tests/oamtestkit-driver/Driver.cpp
@@ -1,6 +1,52 @@
-#include <cppoam/hello.h>
+#include <iostream>
+#include <tuple>
+#include <vector>
+
+#include <msgpack.hpp>
+
+#include <cppoam/MsgPackParser.h>
+#include <cppoam/ProgramRef.h>
+
+bool readTestInputFromStdin(msgpack::object_handle &handle) {
+  msgpack::unpacker unp;
+  unp.reserve_buffer(800);
+
+  const char *bufferBegin = unp.buffer();
+
+  do {
+    *unp.buffer() = std::cin.get();
+    unp.buffer_consumed(1);
+  } while (!unp.next(handle));
+
+  return (unp.buffer() - bufferBegin) > 1;
+}
+
+void sendTestResultToStdout(int result) {
+  using Response_t = std::tuple<
+    std::vector<std::vector<int>>, // values
+    std::vector<int>,              // coeffects
+    std::vector<int>               // killed coeffects
+  >;
+
+  Response_t response {{{0, result}}, {}, {}};
+  msgpack::pack(std::cout, response);
+}
 
 int main(int argc, char **argv) {
-  cppoam::hello();
+  msgpack::object_handle handle;
+
+  while (readTestInputFromStdin(handle)) {
+    cppoam::MsgPackParser parser(std::move(handle));
+
+    if (parser.parseMessageType() == cppoam::MsgPackParser::Program) {
+      ProgramRef program = parser.parseProgram();
+
+      // TODO
+
+      int res = 3;
+      sendTestResultToStdout(res);
+    }
+  }
+
   return 0;
 }
