601e9f5948e616dd023f8729d8d30a7a5d402383
stefan.graenitz <stefan.graenitz@gmail.com>
2018-03-14 12:22:28 +0100

    XRAY2-13 [gtest] Prepare test to evaluate expression '1 | 2'

--- tests/gtest-driver/TestMsgPackParser.cpp
+++ tests/gtest-driver/TestMsgPackParser.cpp
@@ -3,7 +3,7 @@
 
 #include <gtest/gtest.h>
 
-TEST(TestMsgPackParser, Parse1plus2)
+TEST(TestMsgPackParser, Parse_1plus2)
 {
   msgpack::sbuffer buffer;
   msgpack::packer<msgpack::sbuffer> pk(&buffer);
@@ -84,4 +84,53 @@ TEST(TestMsgPackParser, Parse1plus2)
   EXPECT_EQ(2, program.getNumFunctions());
   EXPECT_EQ(5, program.getFunction(0).getNumInstructions());
   EXPECT_EQ(1, program.getFunction(1).getNumInstructions());
+}
+
+TEST(TestMsgPackParser, Evaluate_1parallel2)
+{
+  msgpack::sbuffer buffer;
+  msgpack::packer<msgpack::sbuffer> pk(&buffer);
+
+  // OAM program for expression: '1 | 2'
+  pk.pack_array(2); {
+    pk.pack_int(0);
+    pk.pack_array(2); {
+      pk.pack_array(0); {
+      }
+      pk.pack_array(1); {
+        pk.pack_array(9); {
+          pk.pack_int(0);
+          pk.pack_int(3);
+          pk.pack_int(8);
+          pk.pack_int(1);
+          pk.pack_int(8);
+          pk.pack_int(2);
+          pk.pack_int(0);
+          pk.pack_int(1);
+          pk.pack_int(0);
+        }
+      }
+    }
+  }
+
+  msgpack::unpacker pac;
+  pac.reserve_buffer(buffer.size());
+  memcpy(pac.buffer(), buffer.data(), buffer.size());
+  pac.buffer_consumed(buffer.size());
+
+  msgpack::object_handle handle;
+  pac.next(handle);
+
+  std::ostringstream ostr;
+  ostr << handle.get();
+
+  ASSERT_EQ("[0, [[], [[0, 3, 8, 1, 8, 2, 0, 1, 0]]]]", ostr.str());
+
+  cppoam::MsgPackParser parser(std::move(handle));
+  EXPECT_EQ(cppoam::MsgPackParser::ByteCode, parser.parseMessageType());
+
+  cppoam::ProgramRef program = parser.parseProgram();
+  EXPECT_EQ(0, program.getNumFFI());
+  EXPECT_EQ(1, program.getNumFunctions());
+  EXPECT_EQ(3, program.getFunction(0).getNumInstructions());
 }
